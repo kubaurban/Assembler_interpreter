9892a7ad8a31b2de655ca323d8e3740c77258727
kubatuba01 <kuba.urban01@wp.pl>
2020-10-27 12:23:36 +0100

    Przygotuj podstawy do interpretowania rozkazów. Nadaj kod rozkazu, stwórz funkcje give_ordCode(). W pełni obsłuż rozkazy typu rej-rej. Usuń ściągawkę Order_codes.c Uprość funkcje interpret_div(). Dostosuj funkcje into_hex() do zapisu kodu na 4,2,1 lub połowie bajtu.

--- Interpreter.c
+++ Interpreter.c
@@ -6,91 +6,215 @@
 int N = 9; /*odleglosc znaku '*' od nadanej w dyrektywie o postaci
 				... <liczba_komorek_pamieci>*INTEGER(<liczba_calkowita>) do liczby_calkowitej */
 int i, j, k;
+char z;
 char word[9];
-char word8[9] = { '0','0','0','0','0','0','0','0','\0' };
-char word4[5] = { '0','0','0','0','\0' };
-char emptyWord[9] = { '~', '~',  '~',  '~',  '~',  '~',  '~',  '~', '\0' };
+char word8[9]; // slowo pamieci 4B - 32 bity, 8 bitów szesnastkowo
+char word4[5]; // pamiec 2B - 16 bitów, 4 bity szesnastkowo
+char word2[3]; // jeden bajt - 8 bitów, 2 bity szesnastkowo
+char word1[2]; // jeden polbajt - 4 bity, 1 bit szesnastkowo
+char emptyWord8[9] = { '~', '~',  '~',  '~',  '~',  '~',  '~',  '~', '\0' };
 char* temp1;
 char* temp2;
 char buffer[255];
+char buffer2[255];
 
 
 char* into_hex(const char*, int);
+void give_ordCode(const char*);
+
 void interpret_div(const char* div[]) {
 	temp1 = strchr(div[2], 42); //zwraca wskaznik na pozycje gdzie znajduje sie znak '*' (kod ASCII = 42), lub wartosc NULL jesli nie wystepuje
-	if (strcmp(div[1], "DC") == 0)
+	if (temp1 == NULL)
 	{
-		if (temp1 == NULL)		/* obsluga dyrektyw postaci ... DC INTEGER(<liczba_calkowita>) */
+		if (strcmp(div[1], "DC") == 0)		/* obsluga dyrektyw postaci ... DC INTEGER(<liczba_calkowita>) */
 		{
-			strncat_s(buffer, 255, div[2] + N - 1, strlen(div[2]) - N);
+			strncat_s(buffer, 255, div[2] + N - 1, strlen(div[2]) - N); // wycina fragment <liczba_komorek_pamieci> i zapisuje do buffer
 			printf_s("%s\n", into_hex(buffer, 8)); //tutaj zapisz do pliku 
-			memset(buffer, 0, 255 * sizeof(char));
+			memset(buffer, 0, 255 * sizeof(char)); // czysci tablice buffer
 		}
-
-		else					/* obsluga dyrektyw postaci ... DC <liczba_komorek_pamieci>*INTEGER(<liczba_calkowita>) */
+		else								/* obsluga dyrektyw postaci ... DS INTEGER */
 		{
-			strncat_s(buffer, 255, div[2], strlen(div[2]) - strlen(temp1)); // wycina fragment <liczba_komorek_pamieci> i zapisuje do buffer
-			k = atoi(buffer); // liczba k reprezentuje liczbe_komorek_pamieci
-			memset(buffer, 0, 255 * sizeof(char));
+			printf_s("%s\n", emptyWord8); //tutaj zapisz do pliku 
+		}
+	}
+	else 
+	{
+		strncat_s(buffer, 255, div[2], strlen(div[2]) - strlen(temp1)); // wycina fragment <liczba_komorek_pamieci> i zapisuje do buffer
+		k = atoi(buffer); // liczba k reprezentuje liczbe_komorek_pamieci
+		memset(buffer, 0, 255 * sizeof(char));
+
+		if (strcmp(div[1], "DC") == 0)		/* obsluga dyrektyw postaci ... DC <liczba_komorek_pamieci>*INTEGER(<liczba_calkowita>) */
+		{
 			strncat_s(buffer, 255, (temp1 + N), strlen(temp1) - N - 1); /* wycina fragment <liczba_calkowita> i zapisuje do buffer*/
-			for (j = 0; j < k; j++) 
+			for (j = 0; j < k; j++)
 			{
 				printf_s("%s\n", into_hex(buffer, 8)); //tutaj zapisz do pliku
 			}
 			memset(buffer, 0, 255 * sizeof(char));
 		}
-	}
-
-	else 
-	{
-		if (temp1 == NULL)		/* obsluga dyrektyw postaci ... DS INTEGER */
+		else								/* obsluga dyrektyw postaci ... DS <liczba_komorek_pamieci>*INTEGER */
 		{
-			printf_s("%s\n", emptyWord); //tutaj zapisz do pliku 
-		}
-
-		else					/* obsluga dyrektyw postaci ... DS <liczba_komorek_pamieci>*INTEGER */
-		{
-			strncat_s(buffer, 255, div[2], strlen(div[2]) - strlen(temp1)); // wycina fragment <liczba_komorek_pamieci> i zapisuje do buffer
-			k = atoi(buffer); // liczba k reprezentuje liczbe_komorek_pamieci
-			memset(buffer, 0, 255 * sizeof(char));
 			for (j = 0; j < k; j++)
 			{
-				printf_s("%s\n", emptyWord); //tutaj zapisz do pliku
+				printf_s("%s\n", emptyWord8); //tutaj zapisz do pliku
 			}
 		}
 	}
 }
 
-char* interpret_ord(char** ord) {
-	char* bin;
-	temp1 = strchr(ord[2], 44); //zwraca wskaznik na pozycje gdzie znajduje sie znak ',' (kod ASCII = 44), lub wartosc NULL jesli nie wystepuje
-	// tutaj nastapi nadanie do buffer kodu rozkazu
+void interpret_ord(char* ord[]) {
+	give_ordCode(ord[1]);		// nadanie do buffer kodu rozkazu
+
+	temp1 = strchr(ord[2], 44); //zwraca wskaznik na pozycje gdzie znajduje sie znak ',' (kod ASCII = 44), lub wartosc NULL jesli nie wystepuje					
 	if (temp1 != NULL) // obsluga rozkazow arytmetycznych i zapisywania wartosci 
 	{
-		//strncat_s()
-		//if()
+		if (strlen(ord[1]) == 2 && *(ord[1] + 1) == 'R') // obsluga rozkazow typu rejestr - rejestr
+		{
+			for (i = 0; (*ord[2]) != ','; i++)
+			{
+				buffer2[i] = *(ord[2]);
+				ord[2]++;
+			}
+			strcat_s(buffer, 4, into_hex(buffer2, 1)); // wyodrêbnienie pierwszego rejestru
+			memset(buffer2, 0, 255 * sizeof(char)); 
+
+			ord[2]++;
+			for (i = 0; (*ord[2]) != '\0'; i++)
+			{
+				buffer2[i] = *(ord[2]);
+				ord[2]++;
+			}
+			strcat_s(buffer, 5, into_hex(buffer2, 1)); // wyodrêbnienie drugiego rejestru
+			memset(buffer2, 0, 255 * sizeof(char));
+
+			printf("%s", buffer); // zapisz do pliku
+		}
+		else // obsluga rozkazow typu rejestr - pamiec
+		{
+			printf("%s", buffer); // zapisz do pliku
+
+		}
 	}
 
 	else // obsluga rozkazow skoku
 	{
-
+		printf("%s", buffer); // zapisz do pliku
 	}
+	memset(buffer, 0, 255 * sizeof(char));
 }
 
-char* into_hex(const char* dec, int bit_amount) // zamienia ciag znakow zawierajacy decymalna liczbe na ciag znakow w reprezentujacy te liczbe heksadecymalnie
+void zeruj_slowo(int b, char* slowo)
 {
-	sprintf_s(word, bit_amount, "%x", atoi(dec));
-	if (bit_amount == 8)
+	for (i = 0; i < b; i++)
 	{
-		temp2 = word8;
+		slowo[i] = '0';
+	}
+}
+char* into_hex(const char* dec, int hex_bit_amount) // zamienia ciag znakow zawierajacy decymalna liczbe na ciag znakow w reprezentujacy te liczbe heksadecymalnie
+{
+	sprintf_s(word, hex_bit_amount + 1, "%llx", atoll(dec));
+	switch (hex_bit_amount)
+	{
+		case 8:
+			temp2 = word8;
+			break;
+		case 4:
+			temp2 = word4;
+			break;
+		case 2:
+			temp2 = word2;
+			break;
+		case 1:
+			temp2 = word1;
+			break;
+	}
+	zeruj_slowo(hex_bit_amount, temp2);
+	for (i = 0; i < strlen(word); i++)
+	{
+		temp2[hex_bit_amount - 1 - i] = word[strlen(word) - i - 1];
+	}
+	return temp2;
+}
+void give_ordCode(const char* ord_sign) 
+{
+	if (strlen(ord_sign) == 2)
+	{
+		z = *(ord_sign + 1);
+		if (z == 'R')
+		{
+			z = *ord_sign;
+			switch (z)
+			{
+			case 'A':
+				temp2 = into_hex("16", 2); // AR 
+				break;
+			case 'S':
+				temp2 = into_hex("18", 2); // SR
+				break;
+			case 'M':
+				temp2 = into_hex("20", 2); // MR
+				break;
+			case 'D':
+				temp2 = into_hex("22", 2); // DR
+				break;
+			case 'C':
+				temp2 = into_hex("24", 2); // CR
+				break;
+			default:
+				temp2 = into_hex("241", 2); // LR
+				break;
+			}
+		}
+		else
+		{
+			switch (z)
+			{
+			case 'Z':
+				temp2 = into_hex("225", 2); // JZ
+				break;
+			case 'N':
+				temp2 = into_hex("227", 2); // JN
+				break;
+			case 'P':
+				temp2 = into_hex("226", 2); // JP
+				break;
+			case 'T':
+				temp2 = into_hex("243", 2); // ST
+				break;
+			case 'A':
+				temp2 = into_hex("242", 2); // LA
+				break;
+			default:
+				break;
+			}
+		}
 	}
 	else
 	{
-		temp2 = word4;
+		switch (*ord_sign)
+		{
+		case 'A':
+			temp2 = into_hex("209", 2); // A
+			break;
+		case 'S':
+			temp2 = into_hex("211", 2); // S
+			break;
+		case 'M':
+			temp2 = into_hex("213", 2); // M
+			break;
+		case 'D':
+			temp2 = into_hex("215", 2); // D
+			break;
+		case 'C':
+			temp2 = into_hex("217", 2); // C
+			break;
+		case 'J':
+			temp2 = into_hex("224", 2); // J
+			break;
+		default:
+			temp2 = into_hex("240", 2); // L
+			break;
+		}
 	}
-	for (i = 0; i < strlen(word); i++)
-	{
-		temp2[bit_amount - 1 - i] = word[strlen(word) - i - 1];
-	}
-	return temp2;
+	strcat_s(buffer, 3, temp2);
 }
--- Main.c
+++ Main.c
@@ -3,22 +3,23 @@
 #define DATASECTION_REGISTRY = "0000000f";
 #define ORDERSSECTION_REGISTRY = "0000000e";
 
-const char* div1[] = { "dziewiec",	"DC",	"INTEGER(9)" };
-const char* div2[] = { "piec",		"DC",	"INTEGER(5)" };
+const char* div1[] = { "dziewiec",	"DC",	"INTEGER(4294967295)" };
+const char* div2[] = { "piec",		"DC",	"INTEGER(0)" };
 const char* div3[] = { "trzy",		"DC",	"INTEGER(3)" };
-const char* div4[] = { "wektor",	"DC",	"15*INTEGER(200)" };
+const char* div4[] = { "wektor",	"DC",	"15*INTEGER(4294967295)" };
 const char* div5[] = { "wynik",		"DS",	"INTEGER" };
-const char* div6[] = { "wynik_wek",	"DS",	"8*INTEGER" };
+const char* div6[] = { "wynik_wek",	"DS",	"5*INTEGER" };
 
 const char* ord1[] = { "r1",	 "L",	"2,dziewiec" };
 const char* ord2[] = { "r2",	 "S",	"2,piec" };
 const char* ord3[] = { "r3",	 "S",	"2,trzy" };
 const char* ord4[] = { "r4",	 "ST",	"2,wynik" };
-const char* ord5[] = { "r5",	 "AR",	"2,3" };
+const char* ord5[] = { "r5",	 "L",	"2,3" };
 const char* ord6[] = { "r6",	 "JN",	"r1" };
 
 
 void interpret_div(const char*[]);
+void interpret_ord(const char*[]);
 main(int argc, char** argv)
 {
 	char* dsp; // data_section_pointer start
@@ -30,5 +31,10 @@ main(int argc, char** argv)
 	interpret_div(div4);
 	interpret_div(div5);
 	interpret_div(div6);
+	printf("%s", "\n");
+	interpret_ord(ord5);
+
+	
+	printf("%s", "\n");
 }
 
